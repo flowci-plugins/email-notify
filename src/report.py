5530790bca7c29a23bbb16436b227949726c5663
gy2006 <32008001@qq.com>
2019-11-17 01:09:30 +0100

    update

--- src/report.py
+++ src/report.py
@@ -3,7 +3,7 @@ from email import encoders
 from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
-from util import getVar, fetchCredential
+from util import FlowName, BuildNumber, getVar, fetchCredential
 
 SmtpAddr = getVar('FLOWCI_EMAIL_SMTP')
 IsSSL = getVar('FLOWCI_EMAIL_SSL')
@@ -17,28 +17,35 @@ def createServer():
         
     return smtplib.SMTP(SmtpAddr)
 
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
+
 def createHtml():
-    msg = MIMEText('hello, send by Python...', 'html', 'utf-8')
-    msg['From'] = FromAddr
+    msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+    msg['From'] = _format_addr('flow.ci <%s>' % FromAddr)
     msg['To'] = ToAddr
-    msg['Subject'] = Header('flow.ci report', 'utf-8').encode()
+
+    subject = "job {} - #{}".format(FlowName, BuildNumber)
+    msg['Subject'] = Header(subject, 'utf-8').encode()
+    return msg
 
 def fetchFlowUsers():
     global ToAddr
     # TODO: fetch flow userlist from api
     pass
 
-def fetchCredentail():
-    pass
-
 def send():
     server = createServer()
+    server.set_debuglevel(1)
 
     if ToAddr == 'FLOW_USERS':
         fetchFlowUsers()
 
     if Credential != None:
-        c = fetchCredentail()
+        c = fetchCredential(Credential)
         server.login(c['pair']['username'], c['pair']['password'])
 
     msg = createHtml()
